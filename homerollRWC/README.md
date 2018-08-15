2b75768b3fcac2d8248a448b88aa337b89ca8096
sarenord <ajsheld12@gmail.com>
2018-08-15 11:16:27 -0400

    added pywal functionality, ironed out kinks, added config, updated readme

--- homerollRWC/README.md
+++ homerollRWC/README.md
@@ -1,1 +1,6 @@
 Just my own basic implementation of a reddit wallpaper changer. It takes the top post of r/wallpapers and sets it as your wallpaper
+
+### Requirements
+* oomox
+* [pywal](www.github.com/dylanaraps/pywal)
+* praw
--- homerollRWC/main.py
+++ homerollRWC/main.py
@@ -1,13 +1,26 @@
 import praw
 import urllib.request as urllib
 from subprocess import call
-paperpath = "/home/sarenord/.config/i3/wallpaper.png"
-cachedir = "/home/sarenord/Pictures/wallpapers/"
-reddit = praw.Reddit()
-subreddit = reddit.subreddit('wallpapers')
-for i in subreddit.top('day'):
-    post = i
-    break
+import time
+import json
+import os
+import pywal
 
-urllib.urlretrieve(post.url, paperpath)
-call(["cp", paperpath, cachedir+post.title])
+config = json.load(open("config.json"))
+paperpath = config["paperpath"]
+cachedir = config["cachedir"]
+# date = time.ctime()[9].split(' ')[1:3])
+date = time.ctime(os.stat(paperpath)[9]).split(' ')[1:3]
+
+if date != time.ctime(time.time())[1:3]:
+	reddit = praw.Reddit()
+	subreddit = reddit.subreddit(config["subreddit"])
+	for i in subreddit.top('day'):
+	    post = i
+	    break
+	
+	urllib.urlretrieve(post.url, paperpath)
+	call(["cp", paperpath, cachedir+post.title])
+
+call(["wal", "-i", paperpath, "-g"])
+
