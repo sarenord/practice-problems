ef8ae949fe57125bec29eb311e1f85516f26a1b8
sarenord <ajsheld12@gmail.com>
2018-08-14 22:24:56 -0400

    sorta works

--- homerollRWC/main.py
+++ homerollRWC/main.py
@@ -1,11 +1,13 @@
 import praw
 import urllib.request as urllib
-
+from subprocess import call
+paperpath = "/home/sarenord/.config/i3/wallpaper.png"
+cachedir = "/home/sarenord/Pictures/wallpapers/"
 reddit = praw.Reddit()
 subreddit = reddit.subreddit('wallpapers')
-posts = []
 for i in subreddit.top('day'):
-    posts.append(i.url)
-url = posts[0]
+    post = i
+    break
 
-urllib.urlretrieve(url, "/home/sarenord/.config/i3/wallpaper.png")
+urllib.urlretrieve(post.url, paperpath)
+call(["cp", paperpath, cachedir+post.title])
