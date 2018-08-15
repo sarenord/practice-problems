eac569c17c57f0ff808b21282e71506a4200a7e7
sarenord <ajsheld12@gmail.com>
2018-08-15 11:27:17 -0400

    sorting

---
+++ challenges/palindrome detector/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"strings"
+	"os"
+	"fmt"
+)
+
+func main() {
+	counter := 0
+	inp := os.Args[1]
+	var plc []string
+	plc = strings.Split(inp, "")
+	var plc_inverse []string
+	for i := 1; i <= (len(plc)); i++ {
+	plc_inverse = append(plc_inverse, plc[len(plc)-i])
+	}
+	for in, v := range plc {
+		if v == plc_inverse[in]{
+			counter++
+		}
+	}
+	if counter == len(plc){ 
+		fmt.Println(inp + " is a palindrome!")
+	} else { 
+		fmt.Println(inp + " is not a palindrome")
+	}
+}
